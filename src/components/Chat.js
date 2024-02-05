34c396863dbdc6d5de059937b5ebdb68da7f55ae
Katell <katell.quemeneur@gmail.com>
2024-02-05 20:59:58 +0100

    feat: implement working live chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './Chat.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 class Chat extends React.Component {
 
   constructor(props) {
     super(props)
-    /*this.state = {
-      item: 0,
-    }*/
+    this.state = {
+      messages: [],
+      input: ''
+    }
+    this.sendMessage = this.sendMessage.bind(this);
+    this.setInput = this.setInput.bind(this)
+  }
+  
+  setInput(value){
+    this.setState({ input: value });
+  }
+
+  sendMessage(e) {
+    e.preventDefault();
+    socket.emit('chat message', this.state.input);
+    this.setInput('');
+  };
+
+  componentDidMount() {
+    // Subscribe to 'chat message' event
+    socket.on('chat message', (msg) => {
+      this.setState((prevState) => ({
+        messages: [...prevState.messages, msg]
+      }));
+    });
+  }
+
+  componentWillUnmount() {
+    // Unsubscribe from 'chat message' event when component unmounts
+    socket.off('chat message');
   }
 
   render() {
@@ -16,13 +46,20 @@ class Chat extends React.Component {
         <div className="messages-container">
           <p className="external-message message">He</p>
           <p className="internal-message message">Oh</p>
+          {this.state.messages.map((message, index) => (
+            <p key={index} className="external-message message">{message}</p>
+          ))}
 
         </div>
-        <form className="message-input">
-          <input name="message" type="text"></input>
+        <form className="message-input" onSubmit={this.sendMessage}>
+          <input
+            name="message"
+            type="text"
+            value={this.input}
+            onChange={(e) => this.setInput(e.target.value)}></input>
           <button type="submit">Envoyer</button>
         </form>
-  
+
       </div>
     );
   }
